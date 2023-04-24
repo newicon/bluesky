5c57f7e1d2266e8aedd31e1451080b8e13444331
Steve O'Brien <steve@newicon.net>
2023-04-25 00:26:15 +0100

    basic page layout cmp

--- src/components/PageLayout.vue
+++ src/components/PageLayout.vue
@@ -11,14 +11,9 @@
 		</div>
 		
 		<!-- (flex child) will grow to fill space -->
-		<router-view v-slot="{ Component }" :name="transitionName" appear>
-			<transition>
-				<div class="relative grow">
-					<component :is="Component" />
-					{{ route }}
-				</div>
-			</transition>
-		</router-view>
+		<div class="relative grow">
+			<slot />
+		</div>
 
 		<!-- (flex child) nav will be pushed to the bottom -->
 		<div class="z-50">
@@ -26,16 +21,32 @@
 		</div>
 	</div>
 </template>
-<script lang="ts">
+
+<script setup lang="ts">
 import { useRoute } from 'vue-router'
-import { on } from 'vue'
-export default {
-	setup() {
-		const route = useRoute();
-		return {
-			route
-		}
-	},
-}
+import { computed } from 'vue'
+
+const route = useRoute();
+
+const transitionName = computed(() => {
+	return route.meta.transition
+});
+
 </script>
 
+<style>
+.slide-leave-active,
+.slide-enter-active {transition: .5s cubic-bezier(0.1, 0.7, 0.1, 1);position:absolute;top:0;left:0;right:0;bottom:0;}
+.slide-leave-from { transform: translateX(0) }
+.slide-leave-to { transform: translateX(-100%) }
+.slide-enter-from { transform: translateX(100%) }
+.slide-enter-to { transform: translateX(0) }
+
+
+.slideback-leave-active,
+.slideback-enter-active {transition: .5s cubic-bezier(0.1, 0.7, 0.1, 1);position:absolute;top:0;left:0;right:0;bottom:0;}
+.slideback-enter-from { transform: translateX(-100%) }
+.slideback-enter-to { transform: translateX(0) }
+.slideback-leave-from { transform: translateX(0) }
+.slideback-leave-to { transform: translateX(100%) }
+</style>
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,23 +1,60 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import type { RouteRecordRaw } from 'vue-router';
 import HomeView from '../views/HomeView.vue'
 
+enum RouteNames {
+	Home = "Home",
+	About = "About",
+}
+interface RouteInterface {
+	[key: string]: RouteRecordRaw
+}
+
+const routes: RouteInterface = {
+	[RouteNames.Home]: {
+		path: '/',
+		name: RouteNames.Home,
+		component: HomeView,
+		meta: { transition: 'slide' },
+	},
+	[RouteNames.About]: {
+		path: '/about',
+		name: RouteNames.About,
+		// route level code-splitting
+		// this generates a separate chunk (About.[hash].js) for this route
+		// which is lazy-loaded when the route is visited.
+		component: () => import('../views/AboutView.vue'),
+		meta: { transition: 'slide' },
+	}
+}
 const router = createRouter({
 	history: createWebHistory(import.meta.env.BASE_URL),
 	routes: [
 		{
 			path: '/',
-			name: 'home',
-			component: HomeView
+			name: RouteNames.Home,
+			component: HomeView,
+			meta: { transition: 'slide' },
 		},
 		{
 			path: '/about',
-			name: 'about',
+			name: RouteNames.About,
 			// route level code-splitting
 			// this generates a separate chunk (About.[hash].js) for this route
 			// which is lazy-loaded when the route is visited.
-			component: () => import('../views/AboutView.vue')
+			component: () => import('../views/AboutView.vue'),
+			meta: { transition: 'slide' },
 		}
 	]
 })
+
+// router.afterEach((to, from) => {
+// 	if (to.meta.transition === 'slide' || from.meta.transition === 'slide') {
+// 		const toDepth = to.path.split('/').length
+// 		const fromDepth = from.path.split('/').length
+// 		to.meta.transitionName = toDepth < fromDepth ? 'slideback' : 'slide'
+// 	}
+// })
+
 
 export default router
